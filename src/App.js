c129c94811c80295d2c1e734c33a67388f50a6f7
Maricarmen <nemracira1718@gmail.com>
2024-04-15 15:52:19 -0500

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="body">
+      <div className='app__container'>
+        <Header/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
